7712868604a1b110f573cd1a76b48bb87561f11e
Laurence <snowsnooks@gmail.com>
2022-03-31 11:28:22 +0100

    Update maze-navigator.py

--- maze-navigator.py
+++ maze-navigator.py
@@ -155,7 +155,7 @@ class Follower:
 
 
     def image_callback(self, msg):
-                cv2.namedWindow("Image window", 1)
+        cv2.namedWindow("Image window", 1)
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError, e:
