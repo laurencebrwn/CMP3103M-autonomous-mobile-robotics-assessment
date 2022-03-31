9b3c7f5ddf5c674e299ebb424cefc9aae87c7a3b
Laurence <snowsnooks@gmail.com>
2022-03-31 23:04:54 +0100

    fixed wall loop

--- maze-navigator.py
+++ maze-navigator.py
@@ -37,6 +37,7 @@ class Follower:
 
     def red_movement(self, ranges):
         self.twist.linear.x = 0
+        self.still_turning = True
         if self.moving_from_red[1] == "right":
             self.twist.angular.z = 1
         else:
