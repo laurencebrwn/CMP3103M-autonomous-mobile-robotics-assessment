f96b66fef523f7d03d1a7ed0a9e707f704e8173d
Laurence <snowsnooks@gmail.com>
2022-04-02 11:02:42 +0100

    Update maze-navigator.py

--- maze-navigator.py
+++ maze-navigator.py
@@ -93,15 +93,13 @@ class Follower:
 
     def blue_movement(self, ranges):
         min_dist = min(ranges)
-        print ranges
-        if min_dist > 1 or self.moving_to_blue[1] == "both":
-            self.normal_movement(ranges)
-        elif min_dist < 0.4:
+        #print ranges
+        if min_dist < 0.4:
             self.still_turning = True
             self.twist.linear.x = 0
             if min_dist < 0.32:
                 self.twist.linear.x = -0.25
-            elif (far_left_dist + left_dist)/2 > (far_right_dist + right_dist)/2:
+            elif self.moving_to_blue[1] == "left":
                 print "turning left"
                 self.twist.angular.z = 0.5
                 self.prev_direction = 'left'
